e3b91bd01aeff6b2f4328fe6875836134a3cefce
mvolchenkova <121573011+mvolchenkova@users.noreply.github.com>
2024-06-05 19:01:12 +0300

    feat: finish page

--- books.js
+++ books.js
@@ -1,3 +1,4 @@
+qwerty();
 document.addEventListener('DOMContentLoaded', function() {
     const searchInput = document.getElementById('search-input');
     const searchButton = document.querySelector('.searchBut');
@@ -6,47 +7,102 @@ document.addEventListener('DOMContentLoaded', function() {
     searchInput.setAttribute('autocomplete', 'off');
 
     searchButton.addEventListener('click', function() {
-        searchBooks();
+        searchBooks(searchInput);
     });
 
     searchInput.addEventListener('keyup', function(event) {
         if (event.key === 'Enter') {
-            searchBooks();
+            searchBooks(searchInput);
         }
     });
 });
 
-function searchBooks() {
-    const searchQuery = document.getElementById('search-input').value;
-    fetch('https://www.googleapis.com/books/v1/volumes?q=' + searchQuery + '&key=AIzaSyC05Ww6RmgFhpq6OA7d9pYyXLGSjLNR9EM')
-        .then(response => response.json())
+function searchBooks(searchInput) {
+    const url = `http://openlibrary.org/search.json?q=${encodeURIComponent(searchInput.value)}`;
+    fetch(url)
+    .then(response => response.json())
         .then(data => {
-            let resultsHTML = '';
-            if (data.items) {
-                data.items.forEach(item => {
-                    const title = item.volumeInfo.title;
-                    const authors = item.volumeInfo.authors ? item.volumeInfo.authors.join(', ') : '';
-                    const imageLink = item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.smallThumbnail : '';
-                    const category = item.volumeInfo.categories ? item.volumeInfo.categories[0] : '';
-                    resultsHTML += `
+            let cardsContainer = document.querySelector('.cards');
+            cardsContainer.innerHTML = '';
+            console.log(data)
+            if (data.docs && data.docs.length > 0) {
+                data.docs.forEach(item => {
+                    let card = document.createElement('div');
+                    const title = item.title || '';
+                    const authors = item.author_name ? item.author_name.join(', ') : '';
+                    const firstEdition = item.first_publish_year || '';
+                    const coverId = item.cover_i;
+                    const imageLink = coverId ? `http://covers.openlibrary.org/b/id/${coverId}-S.jpg` : '';
+                    let category = item.subject ? item.subject[0] : ''; 
+                    if (category) {
+                        let words = category.split(' ');
+                        if (words.length > 4) {
+                            category = words.slice(0, 4).join(' ') + '...';
+                        }
+                    }
+                    card.innerHTML = `
                         <div class="card">
                             <div>
                                 <p class="bookname">${title}</p>
                                 <img src="${imageLink}" alt="${title}" class="cover">
-                                <p class="categories">${category}</p>
+                                <p class="first-edition">${firstEdition}</p>
                                 <p class="authors">${authors}</p>
+                                <p class="category">${category}</p>
                             </div>
                         </div>
                     `;
+                    cardsContainer.appendChild(card);
                 });
             } else {
-                resultsHTML = '<p>No results found.</p>';
+                cardsContainer.innerHTML = '<p>No results found.</p>';
             }
-            document.querySelector('.cards').innerHTML = resultsHTML;
         });
 }
 
-document.querySelector('.searchBut').addEventListener('click', function() {
-    document.getElementById('search-input').value = '';
-    document.getElementById('search-input').placeholder = 'Input book name';
-});
+
+function qwerty(){
+    const url = `http://openlibrary.org/search.json?q=сказки`;
+    fetch(url)
+    .then(response => response.json())
+        .then(data => {
+            let cardsContainer = document.querySelector('.cards');
+            cardsContainer.innerHTML = '';
+            console.log(data)
+            if (data.docs && data.docs.length > 0) {
+                data.docs.forEach(item => {
+                    let card = document.createElement('div');
+                    const title = item.title || '';
+                    const authors = item.author_name ? item.author_name.join(', ') : '';
+                    const firstEdition = item.first_publish_year || '';
+                    const coverId = item.cover_i;
+                    const imageLink = coverId ? `http://covers.openlibrary.org/b/id/${coverId}-S.jpg` : '';
+                    let category = item.subject ? item.subject[0] : ''; 
+                    if (category) {
+                        let words = category.split(' ');
+                        if (words.length > 4) {
+                            category = words.slice(0, 4).join(' ') + '...';
+                        }
+                    }
+                    card.innerHTML = `
+                        <div class="card">
+                            <div>
+                                <p class="bookname">${title}</p>
+                                <img src="${imageLink}" alt="${title}" class="cover">
+                                <p class="first-edition">${firstEdition}</p>
+                                <p class="authors">${authors}</p>
+                                <p class="category">${category}</p> 
+                            </div>
+                        </div>
+                    `;
+                    cardsContainer.appendChild(card);
+                });
+            } else {
+                cardsContainer.innerHTML = '<p>No results found.</p>';
+            }
+        });
+}
+const clear = document.querySelector('.input-button');
+clear.addEventListener('click', function(){
+    const searchInput = document.getElementById('search-input');
+    searchInput.value = '';
+})
